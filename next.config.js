0754a32d0dd9841c23a6f92dffc9d132f23aa132
zev <zev@decent.xyz>
2024-05-17 16:58:31 -0400

    transpile

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
+  transpilePackages: ["@decent.xyz/the-box"],
 };
 
 module.exports = nextConfig;
